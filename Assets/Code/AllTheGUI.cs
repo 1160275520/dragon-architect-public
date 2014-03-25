456ccb5742402ed68405d67f33900eabc62f343e
awb <awb@cs.washington.edu>
2014-03-25 15:23:06 -0700

    super simple robot and GUI button added

---
+++ Assets/Code/AllTheGUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AllTheGUI : MonoBehaviour {
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 150), "Forward"))
+        {
+            var dir = GameObject.Find("Robot").GetComponent<Robot>().forwardVec;
+            Debug.Log(dir);
+            GameObject.Find("Robot").transform.position += dir;
+        }
+    }
+}
---
+++ Assets/Code/Robot.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.ComponentModel;
+
+public class Robot : MonoBehaviour {
+
+    public enum Axis
+    {
+        X, Y, Z
+    }
+
+    public enum Direction
+    {
+        Pos, Neg
+    }
+
+    public static int intFromDirection(Direction d)
+    {
+        if (d == Direction.Neg)
+            return -1;
+        return 1;
+    }
+
+    public Axis axis;
+    public Direction dir;
+
+    public IntVec2 pos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var grid = gameObject.GetComponent<Grid>();
+        transform.position = grid.CenterOfCell(pos);
+	}
+
+    public Vector3 forwardVec
+    {
+        get
+        {
+            switch (axis)
+            {
+                case Axis.X:
+                    return new Vector3(intFromDirection(dir), 0, 0);
+                case Axis.Y:
+                    return new Vector3(0, intFromDirection(dir), 0);
+                case Axis.Z:
+                    return new Vector3(0, 0, intFromDirection(dir));
+                default:
+                    throw new InvalidEnumArgumentException();
+            }
+            
+        }
+    }
+}
