1e6971ef6dbc8be7c49d72f7a01ab0d7400d56dc
edbutler <edbutler@cs.washington.edu>
2014-04-15 13:21:02 -0700

    implemented program <-> json conversion

---
+++ External/Hackcraft/Serialization.fs
@@ -0,0 +1,77 @@
+﻿module Hackcraft.Serialization
+
+open Hackcraft.Ast.Imperative
+
+let private jstr s = Json.String s
+let private jint i = Json.Int i
+let private jarr x = Json.Array (Array.ofSeq x)
+
+let private jarrmap f x = jarr (Seq.map f x)
+
+let JsonOfProgram (program:Program) =
+    let jsonOfObj (o:obj) =
+        match o with
+        | :? int as i -> jint i
+        | _ -> invalidArg "o" (sprintf "cannot json encode object of type '%s'" (o.GetType().Name))
+
+    let jsonOfMeta (meta:Meta) =
+        Json.Object (
+            Map([
+                ("id", jint meta.Id);
+            ])
+        )
+
+    let jsonOfExpr (expr:Expression) =
+        let fields =
+            match expr with
+            | Literal o -> [("type", jstr "literal"); ("value", jsonOfObj o)]
+            | Argument a -> [("type", jstr "argument"); ("index", jint a)]
+        Json.Object (Map.ofList fields)
+
+    let rec jsonOfStmt (stmt:Statement) =
+        let fields =
+            match stmt.Stmt with
+            | Call c -> [("type", jstr "call"); ("proc", jstr c.Proc); ("args", jarrmap jsonOfObj c.Args)]
+            | Repeat r -> [("type", jstr "repeat"); ("stmt", jsonOfStmt r.Stmt); ("numtimes", jsonOfExpr r.NumTimes)]
+            | Command c -> [("type", jstr "command"); ("command", jstr c)]
+        Json.Object (Map.ofList (("meta", jsonOfMeta stmt.Meta) :: fields))
+
+    let jsonOfProc name (proc:Procedure) =
+        Json.Object (
+            Map([
+                ("arity", jint proc.Arity);
+                ("body", jarrmap jsonOfStmt proc.Body);
+            ])
+        )
+
+    Json.Object (Map.map jsonOfProc program.Procedures)
+
+let ProgramOfJson (json:Json.JsonValue) =
+
+    let parseMeta (j:Json.JsonValue) =
+        let jmeta = j.AsObject
+        {Id=jmeta.["id"].AsInt}
+
+    let parseExpr (j:Json.JsonValue) =
+        let jexpr = j.AsObject
+        match jexpr.["type"].AsString with
+        | "literal" -> Literal jexpr.["value"].AsInt
+        | "argument" -> Argument jexpr.["index"].AsInt
+        | t -> invalidArg "j" ("invalid expression type " + t)
+
+    let rec parseStmt (j:Json.JsonValue) =
+        let jstmt = j.AsObject
+        let stmt =
+            match jstmt.["type"].AsString with
+            | "call" -> Call {Proc=jstmt.["proc"].AsString; Args=ImmArr.ofSeq (jstmt.["args"].AsArray |> Seq.map (fun x -> upcast x.AsInt))}
+            | "repeat" -> Repeat {Stmt=parseStmt jstmt.["stmt"]; NumTimes=parseExpr jstmt.["numtimes"]}
+            | "command" -> Command jstmt.["command"].AsString
+            | t -> invalidArg "j" ("invalid statement type " + t)
+        {Meta=parseMeta jstmt.["meta"]; Stmt=stmt}
+
+    let parseProc name (j:Json.JsonValue) =
+        let jproc = j.AsObject
+        let body = Array.map parseStmt jproc.["body"].AsArray
+        {Arity=jproc.["arity"].AsInt; Body=ImmArr.ofSeq body}
+
+    {Procedures=Map.map parseProc json.AsObject}
---
+++ External/Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using Hackcraft.Ast;
+
+namespace Hackcraft
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var prog = new Imperative.Program(Library.Builtins);
+            var str = Json.Format(Serialization.JsonOfProgram(prog));
+            Debug.WriteLine(str);
+            var nprog = Serialization.ProgramOfJson(Json.Parse(str));
+            Debug.WriteLine(Json.Format(Serialization.JsonOfProgram(nprog)));
+        }
+    }
+}
