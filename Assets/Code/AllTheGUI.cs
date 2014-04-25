88d2e6ce25d53219a202b6cfb612c84dcec76a44
edbutler <edbutler@cs.washington.edu>
2014-04-25 16:23:43 -0700

    better camera, now with rotate! also it actually looks at the character ever!

--- Assets/Code/AllTheGUI.cs
+++ Assets/Code/AllTheGUI.cs
@@ -220,6 +220,8 @@ public class AllTheGUI : MonoBehaviour
         //makeButton("Undo", options, () => progman.Undo());
         makeButton("Zoom In", options, () => FindObjectOfType<MyCamera>().Zoom(0.5f));
         makeButton("Zoom Out", options, () => FindObjectOfType<MyCamera>().Zoom(2f));
+        makeButton("Roate Left", options, () => FindObjectOfType<MyCamera>().Rotate(90));
+        makeButton("Roate Right", options, () => FindObjectOfType<MyCamera>().Rotate(-90));
         GUILayout.EndVertical();
         GUILayout.EndArea();
 
--- Assets/Code/MyCamera.cs
+++ Assets/Code/MyCamera.cs
@@ -3,29 +3,28 @@ using System.Collections;
 
 public class MyCamera : MonoBehaviour
 {
-    public float smooth = 1.5f;         // The relative speed at which the camera will catch up.
+    public float TranslationSmoothness = 1.5f;         // The relative speed at which the camera will catch up.
+    public float RotationSmoothness = 5.0f;         // The relative speed at which the camera will catch up.
     public float WobblePeriod = 3.0f;
-    public float WobbleMagnitude = 3.0f;  
-    
+    public float WobbleMagnitude = 3.0f;
+
     private Transform player;           // Reference to the player's transform.
     private Vector3 relCameraPos;       // The relative position of the camera from the player.
     private float relCameraPosMag;      // The distance of the camera from the player.
     private Vector3 newPos;             // The position the camera is trying to reach.
 
-    
-    void Awake ()
-    {
+
+    void Awake() {
         // Setting up the reference.
         player = GameObject.Find("Robot").transform;
-        
+
         // Setting the relative position as the initial relative position of the camera in the scene.
         relCameraPos = transform.position - player.position;
         relCameraPosMag = relCameraPos.magnitude - 0.5f;
     }
-    
-    
-    void FixedUpdate ()
-    {
+
+
+    void FixedUpdate() {
         var t = Time.fixedTime;
         var y = WobbleMagnitude * Mathf.Sin(t * 4 * Mathf.PI / WobblePeriod) * Vector3.up;
         var x = WobbleMagnitude * Mathf.Cos(t * 2 * Mathf.PI / WobblePeriod) * Vector3.right;
@@ -34,67 +33,72 @@ public class MyCamera : MonoBehaviour
 
         // The standard position of the camera is the relative position of the camera from the player.
         Vector3 standardPos = player.position + (relCameraPos + offset);
-        
+
         // The abovePos is directly above the player at the same distance as the standard position.
         Vector3 abovePos = player.position + Vector3.up * relCameraPosMag;
-        
+
         // An array of 5 points to check if the camera can see the player.
         Vector3[] checkPoints = new Vector3[5];
-        
+
         // The first is the standard position of the camera.
         checkPoints[0] = standardPos;
-        
+
         // The next three are 25%, 50% and 75% of the distance between the standard position and abovePos.
         checkPoints[1] = Vector3.Lerp(standardPos, abovePos, 0.25f);
         checkPoints[2] = Vector3.Lerp(standardPos, abovePos, 0.5f);
         checkPoints[3] = Vector3.Lerp(standardPos, abovePos, 0.75f);
-        
+
         // The last is the abovePos.
         checkPoints[4] = abovePos;
-        
+
         // Run through the check points...
-        for(int i = 0; i < checkPoints.Length; i++)
-        {
+        for (int i = 0; i < checkPoints.Length; i++) {
             // ... if the camera can see the player...
-            if(ViewingPosCheck(checkPoints[i]))
+            if (ViewingPosCheck(checkPoints[i]))
                 // ... break from the loop.
                 break;
         }
-        
+
         // Lerp the camera's position between it's current position and it's new position.
-        transform.position = Vector3.Lerp(transform.position, newPos, smooth * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, newPos, TranslationSmoothness * Time.deltaTime);
+
+        SmoothLookAt();
     }
-    
-    
-    bool ViewingPosCheck (Vector3 checkPos)
-    {
+
+
+    private bool ViewingPosCheck(Vector3 checkPos) {
         RaycastHit hit;
-        
+
         // If a raycast from the check position to the player hits something...
-        if(Physics.Raycast(checkPos, player.position - checkPos, out hit, relCameraPosMag))
+        if (Physics.Raycast(checkPos, player.position - checkPos, out hit, relCameraPosMag))
             // ... if it is not the player...
-            if(hit.transform != player)
+            if (hit.transform != player)
                 // This position isn't appropriate.
                 return false;
-        
+
         // If we haven't hit anything or we've hit the player, this is an appropriate position.
         newPos = checkPos;
         return true;
     }
-    
-    
-    void SmoothLookAt ()
-    {
+
+
+    private void SmoothLookAt() {
         // Create a vector from the camera towards the player.
         Vector3 relPlayerPosition = player.position - transform.position;
-        
+
         // Create a rotation based on the relative position of the player being the forward vector.
         Quaternion lookAtRotation = Quaternion.LookRotation(relPlayerPosition, Vector3.up);
 
-        transform.rotation = lookAtRotation;
+        //transform.rotation = lookAtRotation;
 
         // Lerp the camera's rotation between it's current rotation and the rotation that looks at the player.
-        //transform.rotation = Quaternion.Lerp(transform.rotation, lookAtRotation, smooth * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookAtRotation, RotationSmoothness * Time.deltaTime);
+    }
+
+    public void Rotate(float degrees) {
+        Debug.Log(relCameraPos);
+        relCameraPos = Quaternion.AngleAxis(degrees, Vector3.up) * relCameraPos;
+        Debug.Log(relCameraPos);
     }
 
     public void Zoom(float scale) {
