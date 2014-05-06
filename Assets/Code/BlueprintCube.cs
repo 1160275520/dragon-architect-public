5f16c5c0c4652683e8773ace9cf5f21fefda020d
awb <awb@cs.washington.edu>
2014-05-06 13:56:05 -0700

    blueprint prefab with shadow added

---
+++ Assets/Code/BlueprintCube.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlueprintCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var pos = transform.position;
+        transform.FindChild("Shadow").transform.position = new Vector3(pos.x, 0.01f, pos.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
