1dac7721e3890e6eda0855fda9cc41e301215099
edbutler <edbutler@cs.washington.edu>
2014-06-13 11:45:54 -0700

    Move 3rd-part JS libs into html/lib folder.

--- html/customBlocks.js
+++ html/customBlocks.js
@@ -1,26 +1,4 @@
-/**
- * Blockly Apps: Turtle Graphics Blocks
- *
- * Copyright 2012 Google Inc.
- * https://blockly.googlecode.com/
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * @fileoverview Blocks for Blockly's Turtle Graphics application.
- * @author fraser@google.com (Neil Fraser)
- */
+(function(){
 'use strict';
 
 // Extensions to Blockly's language and JavaScript generator.
@@ -218,3 +196,5 @@ Blockly.UnityJSON.stmtToXML = function (stmt, program) {
 Blockly.Connection.prototype.freeze = function () {
     this.frozen = true;
 };
+}());
+
--- html/hackcraft.js
+++ html/hackcraft.js
@@ -1,26 +1,5 @@
-/**
- * Blockly Apps: Hackcraft Graphics
- *
- * Copyright 2012 Google Inc.
- * https://blockly.googlecode.com/
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
-/**
- * @fileoverview JavaScript for Blockly's Hackcraft application.
- * @author fraser@google.com (Neil Fraser)
- */
+var Hackcraft = (function(){
 'use strict';
 
 /**
@@ -32,12 +11,13 @@ Hackcraft.curProgramStr = "";
 Hackcraft.ignoreNextHistory = false;
 
 // Supported languages.
-BlocklyApps.LANGUAGES =
-    ['en'];
+BlocklyApps.LANGUAGES = ['en'];
 BlocklyApps.LANG = BlocklyApps.getLang();
 
+/* XXX edbutler: was this suppossed to be changed? It currently loads a non-existent file, so I commented it out.
 document.write('<script type="text/javascript" src="generated/' +
                BlocklyApps.LANG + '.js"></script>\n');
+ */
 
 // dictionary of custom block xml
 Hackcraft.Commands = {"move2" :  '<block type="Forward"></block> \
@@ -299,8 +279,9 @@ Hackcraft.getXML = function() {
  * set the html for the instructions, sizing the speech buble appropriately
  * if instructions is the empty string, speech bubble is hidden instead
  */
- Hackcraft.setInstructions = function (instructions) {
+Hackcraft.setInstructions = function (instructions) {
     console.log("setting instructions");
+    console.log(instructions);
     var msg = $('#instructions')[0];
     $('#instructions').removeClass("speechBubble");
     msg.style.visibility = "hidden";
@@ -317,4 +298,8 @@ Hackcraft.getXML = function() {
         msg.style.visibility = "hidden";
         msg.innerHTML = "";
     }
- }
+}
+
+return Hackcraft;
+}());
+
