70e28f76c965c11e0f9ff82013e4ee06c54d497e
edbutler <edbutler@cs.washington.edu>
2014-07-01 12:33:14 -0700

    add some program serialization tests

---
+++ External/HackcraftUnitTests/SerializationTest.fs
@@ -0,0 +1,37 @@
+﻿module Hackcraft.UnitTest.SerializationTest
+
+open Xunit
+open Xunit.Extensions
+open FsUnit.Xunit
+open System.IO
+
+module J = Hackcraft.Json
+module S = Hackcraft.Serialization
+
+let runSerializeFromFileTest filename =
+    let json = File.ReadAllText filename |> J.Parse
+    S.JsonOfProgram (S.ProgramOfJson json) |> should equal json
+
+[<Fact>]
+let ``Serialization level_call_01`` () =
+    runSerializeFromFileTest "../../../../Assets/Resources/level_call_01.txt"
+
+[<Fact>]
+let ``Serialization level_repeat_01`` () =
+    runSerializeFromFileTest "../../../../Assets/Resources/level_repeat_01.txt"
+
+[<Fact>]
+let ``Serialization TLMovement2D`` () =
+    runSerializeFromFileTest "../../../../Assets/Resources/TLMovement2D.txt"
+
+[<Fact>]
+let ``Serialization TLMovement3D`` () =
+    runSerializeFromFileTest "../../../../Assets/Resources/TLMovement3D.txt"
+
+[<Fact>]
+let ``Serialization TLMovementArgs`` () =
+    runSerializeFromFileTest "../../../../Assets/Resources/TLMovementArgs.txt"
+
+[<Fact>]
+let ``Serialization TLPlacement`` () =
+    runSerializeFromFileTest "../../../../Assets/Resources/TLPlacement.txt"
