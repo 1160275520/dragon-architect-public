391b36d0574c843fc5220cb7938892bc84119748
edbutler <edbutler@cs.washington.edu>
2014-07-01 15:03:45 -0700

    add another serialization test that doesn't not depend on external files

--- External/HackcraftUnitTests/SerializationTest.fs
+++ External/HackcraftUnitTests/SerializationTest.fs
@@ -7,10 +7,43 @@ open System.IO
 
 module J = Hackcraft.Json
 module S = Hackcraft.Serialization
+module A = Hackcraft.Ast.Imperative
 
 let runSerializeFromFileTest filename =
     let json = File.ReadAllText filename |> J.Parse
     S.JsonOfProgram (S.ProgramOfJson json) |> should equal json
+
+let testProgram = """
+{
+"F1":{"arity":0,"body":[
+    {"args":[{"type":"literal","value":"1"}],"meta":{"id":25},"proc":"Up","type":"call"},
+    {"args":[{"type":"literal","value":"#5cab32"}],"meta":{"id":26},"proc":"PlaceBlock","type":"call"}
+]},
+"F2":{"arity":0,"body":[]},
+"MAIN":{"arity":0,"body":[
+    {"args":[{"type":"literal","value":"5"}],"meta":{"id":21},"proc":"Forward","type":"call"},
+    {"numtimes":{"type":"literal","value":"10"},"meta":{"id":24},"stmt":
+        {"meta":{"id":27},"body":[
+            {"args":[],"meta":{"id":22},"proc":"F1","type":"call"},
+            {"args":[],"meta":{"id":23},"proc":"Left","type":"call"}
+        ],"type":"block"},"type":"repeat"}]}
+}
+"""
+
+[<Fact>]
+let ``Serialization test_parse`` () =
+    let json = J.Parse testProgram
+    let prog = S.ProgramOfJson json
+    prog.Procedures.Count |> should equal 3
+    prog.Procedures.ContainsKey "MAIN" |> should equal true
+    prog.Procedures.ContainsKey "F1" |> should equal true
+    prog.Procedures.ContainsKey "F2" |> should equal true
+
+    let main = prog.Procedures.["MAIN"]
+    main.Arity |> should equal 0
+    main.Body.Length |> should equal 2
+
+    S.JsonOfProgram prog |> should equal json
 
 [<Fact>]
 let ``Serialization level_call_01`` () =
