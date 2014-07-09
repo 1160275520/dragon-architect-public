1eb47231a0a777f4bc7bc4a169d424c8574afd1e
edbutler <devnull@localhost>
2014-07-09 09:33:53 -0700

    fix synchronization problems with build script (turns out 'return' is a thing)

--- html/gulpfile.js
+++ html/gulpfile.js
@@ -6,17 +6,16 @@ var uglify = require('gulp-uglify');
 var minifyHtml = require('gulp-minify-html');
 var minifyCss = require('gulp-minify-css');
 var rev = require('gulp-rev');
-var rimraf = require('gulp-rimraf');
+var rimraf = require('rimraf');
 
 var BUILD_DIR = __dirname + '/../build/';
 
-gulp.task('clean', function() {
-    gulp.src(BUILD_DIR, {read:false})
-        .pipe(rimraf({force:true}));
+gulp.task('clean', function(cb) {
+    rimraf(BUILD_DIR, cb);
 });
 
 gulp.task('usemin', function() {
-    gulp.src('index.html')
+    return gulp.src('index.html')
         .pipe(usemin({
             css: [minifyCss(), 'concat'],
             html: [minifyHtml({empty: true})],
@@ -25,16 +24,22 @@ gulp.task('usemin', function() {
         .pipe(gulp.dest(BUILD_DIR));
 });
 
-gulp.task('copy_static', function() {
-    gulp.src(['generated/**', '!generated/README*'])
+gulp.task('copy_generated', function() {
+    return gulp.src(['generated/**', '!generated/README*'])
         .pipe(gulp.dest(BUILD_DIR + 'generated/'));
+});
 
-    gulp.src('media/**')
+gulp.task('copy_media', function() {
+    return gulp.src(['media/**'])
         .pipe(gulp.dest(BUILD_DIR + 'media/'));
+});
 
-    gulp.src('lib/font-awesome/fonts/**')
+gulp.task('copy_fonts', function() {
+    return gulp.src('lib/font-awesome/fonts/**')
         .pipe(gulp.dest(BUILD_DIR + 'fonts/'));
 });
+
+gulp.task('copy_static', ['copy_generated', 'copy_media', 'copy_fonts']);
 
 gulp.task('default', ['usemin', 'copy_static']);
 
