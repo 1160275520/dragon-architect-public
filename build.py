a0943249ccf27101bd11eaad5d1898e16abcf4d9
edbutler <edbutler@cs.washington.edu>
2014-07-09 00:01:18 -0700

    add gulp invocation to the primary build.py

--- build.py
+++ build.py
@@ -5,20 +5,17 @@ import os
 import subprocess
 
 # build gamelib/ (the F# library)
-
 MSBUILD="C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\MSBuild.exe"
 GAMELIB_SLN = "gamelib/HackcraftNoUnitTest.sln"
-
 subprocess.call([MSBUILD, GAMELIB_SLN, '/p:Configuration=Release'])
 
 # build unity/ (the Unity project)
-
 subprocess.call(['python', 'unity/build.py'])
 
 # build blockly/
-
 subprocess.call(['python', 'build.py'], cwd='blockly/blockly/')
 
 # build html/
-# TODO actually do this!
+subprocess.call(['npm', 'install'], cwd='html/', shell=True)
+subprocess.call(['gulp'], cwd='html/', shell=True)
 
