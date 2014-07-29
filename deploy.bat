2ee11eb2f98be836831a66c0006602111f6b0b6c
edbutler <edbutler@cs.washington.edu>
2014-07-29 11:06:48 -0700

    change deploy script to send to my work desktop

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,7 @@
-"C:\Program Files (x86)\Putty\plink.exe" edbutler@games.cs.washington.edu "rm -rf /var/www/hackcraft/build"
-"C:\Program Files (x86)\Putty\plink.exe" edbutler@games.cs.washington.edu "rm -rf /var/www/hackcraft/latest"
-"C:\Program Files (x86)\Putty\pscp.exe" -r build edbutler@games.cs.washington.edu:/var/www/hackcraft/
-"C:\Program Files (x86)\Putty\plink.exe" edbutler@games.cs.washington.edu "mv /var/www/hackcraft/build /var/www/hackcraft/latest"
+set host=edbutler@volger.cs.washington.edu
+set webroot=/srv/www/hackcraft
+"C:\Program Files (x86)\Putty\plink.exe" %host% "rm -rf %webroot%/build"
+"C:\Program Files (x86)\Putty\plink.exe" %host% "rm -rf %webroot%/latest"
+"C:\Program Files (x86)\Putty\pscp.exe" -r build %host%:%webroot%/
+"C:\Program Files (x86)\Putty\plink.exe" %host% "mv %webroot%/build %webroot%/latest"
 
