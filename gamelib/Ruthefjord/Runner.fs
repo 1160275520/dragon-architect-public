4c20b8171f6661d06125b5f7daa97d6a79671f42
edbutler <edbutler@cs.washington.edu>
2014-08-08 17:51:14 -0700

    added block/instruction limits to sandbox mode

--- gamelib/Ruthefjord/Runner.fs
+++ gamelib/Ruthefjord/Runner.fs
@@ -4,17 +4,21 @@ namespace Ruthefjord
 // But for now it will suffice.
 type LazyProgramRunner (program, initialGrid:GridStateTracker, initialRobot:Robot.IRobot) =
 
+    let MAX_ITER = 20000
+
     let robot = initialRobot.Clone
     let simulator = Simulator.LazySimulator program
     let initialState : Simulator.StepState = {Command=null; LastExecuted=[]; Robot=robot.Clone; Grid=initialGrid.CurrentState}
     let mutable lastStep = initialState
+    let mutable numSteps = 0
 
     member x.InitialState = initialState
 
-    member x.IsDone = simulator.IsDone
+    member x.IsDone = simulator.IsDone || numSteps >= MAX_ITER
 
     member x.UpdateOneStep (grid:GridStateTracker) : Simulator.StepState =
         let step = simulator.Step ()
         robot.Execute grid step.Command
         lastStep <- {Command=step.Command; LastExecuted=step.LastExecuted; Robot=robot.Clone; Grid=grid.CurrentState}
+        numSteps <- numSteps + 1
         lastStep
