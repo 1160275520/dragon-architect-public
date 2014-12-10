0680faa9c046aabfb106e7724b1fbb6b77bb01b9
Aaron Bauer <awb@cs.washington.edu>
2014-12-10 12:59:55 -0800

    forgot a file

---
+++ html/js/db.js
@@ -0,0 +1,15 @@
+var RuthefjordDB = (function(){ "use strict";
+var module = {};
+
+var site = fermata.json("http://localhost:5000")
+
+module.Upload = (function() {
+    var self = {};
+
+    //site('api')('player').get(function (err, data/*, headers*/) { if (!err) {console.log(data);} })
+
+    return self;
+}());
+
+return module;
+}());
