dbbbf4fc4a01c7979a966c7510d8fa74dcc03466
edbutler <devnull@localhost>
2015-03-25 13:51:11 -0700

    change main website config

---
+++ build/config_website_default.js
@@ -0,0 +1,16 @@
+
+var RUTHEFJORD_CONFIG = {
+    server: {
+        url: 'http://gigantor.cs.washington.edu:27246/api',
+        storage: 'session' // can be 'server', 'local', or 'session'
+    },
+    logging: {
+        server_tag: 'DEVELOPMENT_SERVER',
+        game: {name: 'hackcraft', id: 19, skey: "e00fc765bbcc46064b5eb57dbb02bdf2"},
+        category_id: 100
+    },
+    features: { 
+        is_debugging: true
+    },
+};
+
