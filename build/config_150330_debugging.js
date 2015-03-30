3e7180b5efedac777ad6351ade1e2d65584f1a77
edbutler <edbutler@cs.washington.edu>
2015-03-30 14:52:01 -0700

    give fresh cid to experiment

--- build/config_150330_debugging.js
+++ build/config_150330_debugging.js
@@ -7,7 +7,7 @@ var RUTHEFJORD_CONFIG = {
     logging: {
         server_tag: 'DEVELOPMENT_SERVER',
         game: {name: 'hackcraft', id: 19, skey: "e00fc765bbcc46064b5eb57dbb02bdf2"},
-        category_id: 4
+        category_id: 21
     },
     features: {},
     experiment: {
