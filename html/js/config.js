7a65e27ad84893cf6c39e96d818967e24beeb88d
edbutler <edbutler@cs.washington.edu>
2015-04-01 16:00:07 -0700

    change default development config

--- html/js/config.js
+++ html/js/config.js
@@ -1,32 +1,16 @@
 
 var RUTHEFJORD_CONFIG = {
-    //server: {url: 'http://gigantor.cs.washington.edu:27246/api'},
     server: {
         url: 'http://localhost:5000/api',
         storage: 'local' // can be 'server', 'local', or 'session'
     },
     logging: {
-        //server_tag: 'PRODUCTION_SERVER',
         server_tag: 'DEVELOPMENT_SERVER',
-        //proxy_url: 'logging_proxy',
         game: {name: 'hackcraft', id: 19, skey: "e00fc765bbcc46064b5eb57dbb02bdf2"},
-        category_id: 4
+        category_id: 1
     },
-    features: {},
-    experiment: {
-        id: '7e14b455-2eb4-4485-b210-59d97003da8a'
+    features: { 
+        is_debugging: true
     },
-    // these will be promoted to 'features' based on experimental condition
-    feature_conditions: {
-        1: {
-            is_debugging: true,
-            debugging_always: true,
-            workshop_only: true
-        },
-        2: {
-            is_debugging: false,
-            workshop_only: true
-        }
-    }
 };
 
