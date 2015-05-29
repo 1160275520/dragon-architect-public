941e110ecf7b90c0b7f9e33d26b7c5b39e4509c4
Aaron Bauer <awb@cs.washington.edu>
2015-05-28 19:44:39 -0700

    fixing dll references (again, for some reason), resetting config file, adding two new example programs

--- html/js/config.js
+++ html/js/config.js
@@ -1,18 +1,16 @@
 
 var RUTHEFJORD_CONFIG = {
     server: {
-        url: 'http://gigantor.cs.washington.edu:27246/api',
+        url: 'http://localhost:5000/api',
         storage: 'local' // can be 'server', 'local', or 'session'
     },
     logging: {
         server_tag: 'DEVELOPMENT_SERVER',
         game: {name: 'hackcraft', id: 19, skey: "e00fc765bbcc46064b5eb57dbb02bdf2"},
-        category_id: 22
+        category_id: 1
     },
     features: {
         is_debugging: true,
-        debugging_always: true,
-        workshop_only: true
     }
 };
 
