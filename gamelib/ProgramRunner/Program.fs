4e4827e5359b2da7f50457f9290c7129a407cb47
Aaron Bauer <awb@cs.washington.edu>
2015-06-03 20:56:02 -0700

    adding big_cube test program

--- gamelib/ProgramRunner/Program.fs
+++ gamelib/ProgramRunner/Program.fs
@@ -43,12 +43,12 @@ let main argv =
         //let t = time (fun () -> Simulator.SimulateWithRobot program importedModules runner |> ignore) 10
         //printfn "Time with robot: %.3f" t
 
-        let numIter = 50
+        let numIter = 1
 
-        let t = time (fun () -> Simulator.SimulateWithoutRobot program importedModules |> ignore) 3
+        let t = time (fun () -> Simulator.SimulateWithoutRobot program importedModules |> ignore) numIter
         printfn "Original:  %.3f" t
 
-        let t = time (fun () -> Simulator.SimulateWithoutRobotOptimized program importedModules |> ignore) 3
+        let t = time (fun () -> Simulator.SimulateWithoutRobotOptimized program importedModules |> ignore) numIter
         printfn "Optimized: %.3f" t
 
 //        let t = time (fun () ->
@@ -65,54 +65,54 @@ let main argv =
 //                        Simulator.SimulateWithRobot2 program importedModules runner2 |> ignore) numIter
 //        printfn "Grid as Map with Robot: %.3f" t
 
-        let commands = Simulator.SimulateWithoutRobotOptimized program importedModules
-
-        let delta = BasicWorldStateDelta.Create (List.ofArray commands)
-        let startState = {Robot={Position=IntVec3.Zero; Direction=IntVec3.UnitZ}; Grid=(GridStateTracker2 Seq.empty).CurrentState}:BasicWorldState2
-        let t = time (fun () ->
-                        let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
-                        let grid2 = GridStateTracker2 Seq.empty
-                        let runner2 = BasicImperativeRobotSimulator2 (robot, grid2)
-                        for i = 1 to 10 do
-                            for command in commands do
-                                (runner2 :> Robot.IRobotSimulator2).Execute command) 3
-        printfn "Execute every command 10 times: %.3f" t
-        let t = time (fun () ->
-                        let mutable state = startState
-                        for i = 1 to 10 do
-                            state <- (BasicWorldStateDelta.ApplyDelta state delta).Value) 3
-        printfn "Apply delta 10 times: %.3f" t
-        let startState = {Robot={Position=IntVec3.Zero; Direction=IntVec3.UnitZ}; Grid=System.Collections.Generic.Dictionary ()}:BasicWorldState3
-        let t = time (fun () ->
-                        let mutable state = startState
-                        for i = 1 to 10 do
-                            state <- (BasicWorldStateDelta.ApplyDelta3 state delta).Value) 3
-        printfn "Apply delta 10 times: %.3f" t
-
+//        let commands = Simulator.SimulateWithoutRobotOptimized program importedModules
+//
+//        let delta = BasicWorldStateDelta.Create (List.ofArray commands)
+//        let startState = {Robot={Position=IntVec3.Zero; Direction=IntVec3.UnitZ}; Grid=(GridStateTracker2 Seq.empty).CurrentState}:BasicWorldState2
+//        let t = time (fun () ->
+//                        let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
+//                        let grid2 = GridStateTracker2 Seq.empty
+//                        let runner2 = BasicImperativeRobotSimulator2 (robot, grid2)
+//                        for i = 1 to 10 do
+//                            for command in commands do
+//                                (runner2 :> Robot.IRobotSimulator2).Execute command) numIter
+//        printfn "Execute every command 10 times: %.3f" t
+//        let t = time (fun () ->
+//                        let mutable state = startState
+//                        for i = 1 to 10 do
+//                            state <- (BasicWorldStateDelta.ApplyDelta state delta).Value) numIter
+//        printfn "Apply delta 10 times (immutable): %.3f" t
+//        let startState = {Robot={Position=IntVec3.Zero; Direction=IntVec3.UnitZ}; Grid=System.Collections.Generic.Dictionary ()}:BasicWorldState3
+//        let t = time (fun () ->
+//                        let mutable state = startState
+//                        for i = 1 to 10 do
+//                            state <- (BasicWorldStateDelta.ApplyDelta3 state delta).Value) numIter
+//        printfn "Apply delta 10 times (mutable): %.3f" t
+//
         let t = time (fun () -> 
             let grid = GridStateTracker Seq.empty
             let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
             let runner = BasicImperativeRobotSimulator (robot, grid)
-            Simulator.SimulateWithRobot program importedModules runner |> ignore) 3
+            Simulator.SimulateWithRobot program importedModules runner |> ignore) numIter
         printfn "Simulate unoptimized: %.3f" t
         let t = time (fun () -> 
             let cache = Simulator.Dict ()
             let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
             let initState: BasicWorldState2 = {Robot=robot; Grid=Map.empty}
-            Simulator.RunOptimized37 program importedModules Debugger.stateFunctionsNoOp cache initState |> ignore) 3
-        printfn "Simulate optimized: %.3f" t
+            Simulator.RunOptimized37 program importedModules Debugger.stateFunctionsNoOp cache initState |> ignore) numIter
+        printfn "Simulate optimized (no cache): %.3f" t
         let t = time (fun () -> 
             let cache = Simulator.Dict ()
             let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
             let initState: BasicWorldState2 = {Robot=robot; Grid=Map.empty}
-            Simulator.RunOptimized37 program importedModules Debugger.stateFunctions cache initState |> ignore) 3
-        printfn "Simulate optimized: %.3f" t
+            Simulator.RunOptimized37 program importedModules Debugger.stateFunctions cache initState |> ignore) numIter
+        printfn "Simulate optimized (immutable cache): %.3f" t
         let t = time (fun () -> 
             let cache = Simulator.Dict ()
             let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
             let initState: BasicWorldState3 = {Robot=robot; Grid=System.Collections.Generic.Dictionary()}
-            Simulator.RunOptimized37 program importedModules Debugger.stateFunctions2 cache initState |> ignore) 3
-        printfn "Simulate optimized: %.3f" t
+            Simulator.RunOptimized37 program importedModules Debugger.stateFunctions2 cache initState |> ignore) numIter
+        printfn "Simulate optimized (mutable cache): %.3f" t
 
     with e ->
         printfn "An error: %A" e.Message
