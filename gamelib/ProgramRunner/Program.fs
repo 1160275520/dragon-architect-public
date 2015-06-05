2970a6a9b1dd6717c3b55cf3eaffeac77ce4ce7e
edbutler <edbutler@cs.washington.edu>
2015-06-05 16:26:13 -0700

    change import to use new simulator

--- gamelib/ProgramRunner/Program.fs
+++ gamelib/ProgramRunner/Program.fs
@@ -95,11 +95,11 @@ let main argv =
         let numSamples = 5
         let foo = System.Collections.Generic.List (10000000)
 
-        let numStates =
-            let grid = GridStateTracker2 Seq.empty
-            let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
-            let runner = BasicImperativeRobotSimulator2 (robot, grid)
-            (Simulator.SimulateWithRobot2 program importedModules runner).States.Length
+//        let numStates =
+//            let grid = GridStateTracker2 Seq.empty
+//            let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
+//            let runner = BasicImperativeRobotSimulator2 (robot, grid)
+//            (Simulator.SimulateWithRobot2 program importedModules runner).States.Length
 
 //        let t =
 //            time (fun () ->
@@ -109,38 +109,44 @@ let main argv =
 //                Simulator.SimulateWithRobot program importedModules runner |> (fun s -> foo.Add s.States.Length)
 //            ) numSamples
 //        printfn "Simulate unoptimized HTB ALL: %.3f" t
+//        let t =
+//            time (fun () ->
+//                let grid = GridStateTracker Seq.empty
+//                let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
+//                let runner = BasicImperativeRobotSimulator (robot, grid)
+//                Simulator.SimulateWithRobotLastSateOnly program importedModules runner |> (fun s -> foo.Add (if s.Command = null then 1 else 0))
+//            ) numSamples
+//        printfn "Simulate unoptimized HTB LSO: %.3f" t
         let t =
             time (fun () ->
-                let grid = GridStateTracker Seq.empty
                 let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
-                let runner = BasicImperativeRobotSimulator (robot, grid)
-                Simulator.SimulateWithRobotLastSateOnly program importedModules runner |> (fun s -> foo.Add (if s.Command = null then 1 else 0))
+                let runner = BasicRobotSimulator (HashTableGrid (), robot)
+                Simulator.ExecuteToEnd program runner importedModules |> (fun s -> foo.Add s.Grid.Length)
             ) numSamples
-        printfn "Simulate unoptimized HTB LSO: %.3f" t
-        let t =
-            time (fun () ->
-                let grid = GridStateTracker2 Seq.empty
-                let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
-                let runner = BasicImperativeRobotSimulator2 (robot, grid)
-                Simulator.SimulateWithRobot2 program importedModules runner |> (fun s -> foo.Add s.States.Length)
-            ) numSamples
-        printfn "Simulate unoptimized MAP ALL: %.3f" t
-        let t =
-            time (fun () ->
-                let grid = TreeMapGrid ()
-                let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
-                let runner = BasicRobotSimulator (grid, robot)
-                Simulator.CollectAllStates program runner importedModules None |> (fun s -> foo.Add s.Length)
-            ) numSamples
-        printfn "Simulate unoptimized MP2 ALL: %.3f" t
-        let t =
-            time (fun () ->
-                let grid = GridStateTracker2 Seq.empty
-                let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
-                let runner = BasicImperativeRobotSimulator2 (robot, grid)
-                Simulator.SimulateWithRobot2LastStateOnly program importedModules runner |> (fun s -> foo.Add (s :?> BasicWorldState2).Robot.Position.X)
-            ) numSamples
-        printfn "Simulate unoptimized MAP LSO: %.3f" t
+        printfn "Simulate unoptimized HT2 LSO: %.3f" t
+//        let t =
+//            time (fun () ->
+//                let grid = GridStateTracker2 Seq.empty
+//                let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
+//                let runner = BasicImperativeRobotSimulator2 (robot, grid)
+//                Simulator.SimulateWithRobot2 program importedModules runner |> (fun s -> foo.Add s.States.Length)
+//            ) numSamples
+//        printfn "Simulate unoptimized MAP ALL: %.3f" t
+//        let t =
+//            time (fun () ->
+//                let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
+//                let runner = BasicRobotSimulator (TreeMapGrid (), robot)
+//                Simulator.CollectAllStates program runner importedModules None |> (fun s -> foo.Add s.Length)
+//            ) numSamples
+//        printfn "Simulate unoptimized MP2 ALL: %.3f" t
+//        let t =
+//            time (fun () ->
+//                let grid = GridStateTracker2 Seq.empty
+//                let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
+//                let runner = BasicImperativeRobotSimulator2 (robot, grid)
+//                Simulator.SimulateWithRobot2LastStateOnly program importedModules runner |> (fun s -> foo.Add (s :?> BasicWorldState2).Robot.Position.X)
+//            ) numSamples
+//        printfn "Simulate unoptimized MAP LSO: %.3f" t
 //        let t =
 //            time (fun () ->
 //                let cache = Simulator.Dict ()
@@ -175,16 +181,16 @@ let main argv =
             ) numSamples
         printfn "Simulate opt x50 HTB: %.3f" t
 
-        let t =
-            time (fun () ->
-                let cache = Simulator.MutableDict ()
-                let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
-                let initState () : BasicWorldState3 = {Robot=robot; Grid=System.Collections.Generic.Dictionary()}
-                for i = 1 to 50 do
-                    let n = RMath.floori (((float i) / 50.0) * (float (numStates - 1)))
-                    Simulator.RunToState program importedModules Debugger.stateFunctions2 cache (initState ()) n |> (fun s -> foo.Add s.Robot.Position.X)
-            ) numSamples
-        printfn "Simulate jump x50 HTB: %.3f" t
+//        let t =
+//            time (fun () ->
+//                let cache = Simulator.MutableDict ()
+//                let robot:BasicRobot = {Position=IntVec3.Zero; Direction=IntVec3.UnitZ}
+//                let initState () : BasicWorldState3 = {Robot=robot; Grid=System.Collections.Generic.Dictionary()}
+//                for i = 1 to 50 do
+//                    let n = RMath.floori (((float i) / 50.0) * (float (numStates - 1)))
+//                    Simulator.RunToState program importedModules Debugger.stateFunctions2 cache (initState ()) n |> (fun s -> foo.Add s.Robot.Position.X)
+//            ) numSamples
+//        printfn "Simulate jump x50 HTB: %.3f" t
 
         System.IO.File.WriteAllText("temp.txt", (List.ofSeq foo).ToString())
 
