ad15effbd04278bd4fb6148c85460f28ab76140e
Aaron Bauer <awb@cs.washington.edu>
2015-06-10 18:56:23 -0700

    dumber camera is better camera, blockly lib update

--- unity/Assets/Code/MyCamera.cs
+++ unity/Assets/Code/MyCamera.cs
@@ -41,32 +41,34 @@ public class MyCamera : MonoBehaviour
 
 
         // The standard position of the camera is the relative position of the camera from the player.
-        Vector3 standardPos = player.position + (relCameraPos + offset);
+        newPos = player.position + (relCameraPos + offset);
 
         // The abovePos is directly above the player at the same distance as the standard position.
-        Vector3 abovePos = player.position + Vector3.up * relCameraPosMag;
-
-        // An array of 5 points to check if the camera can see the player.
-        Vector3[] checkPoints = new Vector3[5];
-
-        // The first is the standard position of the camera.
-        checkPoints[0] = standardPos;
-
-        // The next three are 25%, 50% and 75% of the distance between the standard position and abovePos.
-        checkPoints[1] = Vector3.Lerp(standardPos, abovePos, 0.25f);
-        checkPoints[2] = Vector3.Lerp(standardPos, abovePos, 0.5f);
-        checkPoints[3] = Vector3.Lerp(standardPos, abovePos, 0.75f);
-
-        // The last is the abovePos.
-        checkPoints[4] = abovePos;
-
-        // Run through the check points...
-        for (int i = 0; i < checkPoints.Length; i++) {
-            // ... if the camera can see the player...
-            if (ViewingPosCheck(checkPoints[i]))
-                // ... break from the loop.
-                break;
-        }
+//        Vector3 abovePos = player.position + Vector3.up * relCameraPosMag;
+//
+//        // An array of 5 points to check if the camera can see the player.
+//        Vector3[] checkPoints = new Vector3[5];
+//
+//        // The first is the standard position of the camera.
+//        checkPoints[0] = standardPos;
+//
+//        // The next three are 25%, 50% and 75% of the distance between the standard position and abovePos.
+//        checkPoints[1] = Vector3.Lerp(standardPos, abovePos, 0.25f);
+//        checkPoints[2] = Vector3.Lerp(standardPos, abovePos, 0.5f);
+//        checkPoints[3] = Vector3.Lerp(standardPos, abovePos, 0.75f);
+//
+//        // The last is the abovePos.
+//        checkPoints[4] = abovePos;
+//
+//        // Run through the check points...
+//        for (int i = 0; i < checkPoints.Length; i++) {
+//            // ... if the camera can see the player...
+//            if (ViewingPosCheck(checkPoints [i])) {
+//                newPos = checkPoints [i];
+//                // ... break from the loop.
+//                break;
+//            }
+//        }
 
         if (ForceFullTranslation) {
             transform.position = newPos;
@@ -116,7 +118,6 @@ public class MyCamera : MonoBehaviour
             }
         }
         // If we haven't hit anything or we've hit the player, this is an appropriate position.
-        newPos = checkPos;
         return true;
     }
 
