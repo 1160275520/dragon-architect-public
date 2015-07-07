89f7b882d951bbefe632ea4858b685cc42b557e0
edbutler <edbutler@cs.washington.edu>
2015-07-07 13:47:24 -0700

    add "unsolvable puzzle" component

---
+++ unity/Assets/Code/Scenes/UnsolvablePuzzle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnsolvablePuzzle : MonoBehaviour {
+
+    private PuzzleHelper lh;
+
+    void Start() {
+        lh = GetComponent<PuzzleHelper>();
+        lh.WinPredicate = () => false;
+    }
+
+    void Update() {
+    }
+}
