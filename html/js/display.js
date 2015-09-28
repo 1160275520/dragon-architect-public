c7eb64acecf100977a1f4d3b29061725ff8792a1
Aaron Bauer <awb@cs.washington.edu>
2015-09-28 16:25:27 -0700

    skybox, antialiasing turned on

--- html/js/display.js
+++ html/js/display.js
@@ -31,11 +31,11 @@ var RuthefjordDisplay = (function() {
 
     module.init = function(parent) {
         scene = new THREE.Scene();
-        camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+        camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1500);
         clock = new THREE.Clock();
         cubes = {};
 
-        renderer = new THREE.WebGLRenderer();
+        renderer = new THREE.WebGLRenderer( {antialias: true} );
         var dims = parent.getBoundingClientRect();
         renderer.setSize(dims.width, dims.height);
         camera.aspect = dims.width / dims.height;
@@ -43,22 +43,55 @@ var RuthefjordDisplay = (function() {
         camera.updateProjectionMatrix();
         parent.appendChild(renderer.domElement);
 
+        // FPS display
         stats = new Stats();
         stats.setMode( 0 ); // 0: fps, 1: ms, 2: mb
-
         // align top-left
         stats.domElement.style.position = 'absolute';
         stats.domElement.style.left = '0px';
         stats.domElement.style.top = '0px';
         parent.appendChild(stats.domElement);
 
+        // skybox
+        var path = "../media/skybox/";
+        var format = ".jpg";
+        // it's not clear to me three js does what it says it does with the six images, but I've got everything lining
+        // up via trial and error
+        var texes = [path + "px" + format, path + "nx" + format,
+            path + "py" + format, path + "ny" + format,
+            path + "pz" + format, path + "nz" + format];
+        var cubeMap = THREE.ImageUtils.loadTextureCube(texes);
+        //cubeMap.format = THREE.RGBFormat;
+        // code from http://blog.romanliutikov.com/post/58705840698/skybox-and-environment-map-in-threejs
+        var shader = THREE.ShaderLib['cube']; // init cube shader from built-in lib
+        shader.uniforms['tCube'].value = cubeMap; // apply textures to shader
+
+        // create shader material
+        var skyBoxMaterial = new THREE.ShaderMaterial( {
+            fragmentShader: shader.fragmentShader,
+            vertexShader: shader.vertexShader,
+            uniforms: shader.uniforms,
+            depthWrite: false,
+            side: THREE.BackSide
+        });
+
+        // create skybox mesh
+        var skybox = new THREE.Mesh(
+            new THREE.CubeGeometry(1000, 1000, 1000),
+            skyBoxMaterial
+        );
+
+        scene.add(skybox);
+
+        // cube geometry, materials
         cubeGeo = new THREE.BoxGeometry(1, 1, 1);
         var tex = THREE.ImageUtils.loadTexture("../media/canvas_cube.png");
         cubeColors.forEach(function (color) {
-            cubeMats.push(new THREE.MeshLambertMaterial({color:color, map:tex}))
+            cubeMats.push(new THREE.MeshLambertMaterial({color:color, map:tex}));
             cubes[color] = {count:0, meshes:[]};
         });
 
+        // ground plane
         var geometry = new THREE.PlaneBufferGeometry(100, 100, 32);
         tex = THREE.ImageUtils.loadTexture("../media/outlined_cube.png");
         tex.wrapS = THREE.RepeatWrapping;
@@ -68,6 +101,7 @@ var RuthefjordDisplay = (function() {
         var plane = new THREE.Mesh( geometry, material );
         scene.add( plane );
 
+        // robot
         geometry = new THREE.SphereGeometry(0.5, 32, 32);
         material = new THREE.MeshLambertMaterial( {color: 0xf56e90} );
         robot = new THREE.Mesh(geometry, material);
@@ -75,23 +109,25 @@ var RuthefjordDisplay = (function() {
         robot.add(robotDir);
         scene.add(robot);
 
+        // lights
         var light = new THREE.DirectionalLight(0xffffff, 1.74);
         //light.position.set(0.32,0.77,-0.56); // rotating 0,0,-1 by 50 about x then 330 about y
         light.position.set(0.56,-0.32,0.77);
         scene.add(light);
         scene.add(new THREE.AmbientLight(0x404040));
 
+        // camera init
         camera.position.copy(relativeCamPos);
         camera.lookAt(new THREE.Vector3(0,0,0));
         module.rotateCamera(-10);
 
-        //plane.rotation.x += Math.PI / 2;
         robot.position.copy(robotOffset);
 
         clock.start();
-        requestAnimationFrame(update);
+        requestAnimationFrame(update); // change to render to omit fps display
     };
 
+    // necessary for fps display
     var update = function () {
         stats.begin();
         render();
@@ -120,7 +156,7 @@ var RuthefjordDisplay = (function() {
         camera.quaternion.slerp(newCamQ, rotationSmoothness * tDelta);
 
         renderer.render( scene, camera );
-        //requestAnimationFrame(render);
+        //requestAnimationFrame(render); // include to omit fps display
     }
 
     // until the Unity backend is discarded, we have to transform the axes it uses to the axes we use
