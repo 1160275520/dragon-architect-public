ef84504bf0db881eef9593fac9c5068aca30e030
Aaron Bauer <awb@cs.washington.edu>
2015-09-28 11:59:57 -0700

    three js now uses right-hand z-up axes, handles different cube colors

--- html/js/display.js
+++ html/js/display.js
@@ -7,10 +7,10 @@ var RuthefjordDisplay = (function() {
     var cubes, robot;
     var WobblePeriod = 4.0;
     var WobbleMagnitude = 0.05;
-    var UP = new THREE.Vector3(0,1,0);
-    var relativeCamPos = new THREE.Vector3(0,12,10);
+    var UP = new THREE.Vector3(0,0,1);
+    var relativeCamPos = new THREE.Vector3(-10,0,12);
     var relativeCamPosMag = relativeCamPos.length() - 0.5; // -0.5 is an undocumented part of unity version, preserving it here
-    var robotOffset = new THREE.Vector3(0.5,1.5,0.5);
+    var robotOffset = new THREE.Vector3(0.5,0.5,1.5);
     var cubeOffset = new THREE.Vector3(0.5,0.5,0.5);
     // the colors are 1-indexed for some reason
     var cubeMats = ["dummy"];
@@ -20,17 +20,25 @@ var RuthefjordDisplay = (function() {
         return deg / 180 * Math.PI;
     }
 
+    /**
+     * AXES
+     * positive x is forward
+     * positive y is left
+     * positive z is up
+     */
+
     module.init = function(parent) {
         scene = new THREE.Scene();
         camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
         clock = new THREE.Clock();
-        cubes = [];
+        cubes = {};
 
         renderer = new THREE.WebGLRenderer();
         var dims = parent.getBoundingClientRect();
         console.log(dims);
         renderer.setSize(dims.width, dims.height);
         camera.aspect = dims.width / dims.height;
+        camera.up.set(0,0,1);
         camera.updateProjectionMatrix();
         parent.appendChild(renderer.domElement);
 
@@ -38,6 +46,7 @@ var RuthefjordDisplay = (function() {
         var tex = THREE.ImageUtils.loadTexture("../media/canvas_cube.png");
         cubeColors.forEach(function (color) {
             cubeMats.push(new THREE.MeshLambertMaterial({color:color, map:tex}))
+            cubes[color] = {count:0, meshes:[]};
         });
 
         var geometry = new THREE.PlaneBufferGeometry(100, 100, 32);
@@ -52,21 +61,21 @@ var RuthefjordDisplay = (function() {
         geometry = new THREE.SphereGeometry(0.5, 32, 32);
         material = new THREE.MeshLambertMaterial( {color: 0xf56e90} );
         robot = new THREE.Mesh(geometry, material);
-        var robotDir = new THREE.ArrowHelper(new THREE.Vector3(0,0,1),new THREE.Vector3(0,0,0),1,0xff0000,0.5,0.2);
+        var robotDir = new THREE.ArrowHelper(new THREE.Vector3(1,0,0),new THREE.Vector3(0,0,0),1,0xff0000,0.5,0.2);
         robot.add(robotDir);
         scene.add(robot);
 
         var light = new THREE.DirectionalLight(0xffffff, 1.74);
-        light.position.set(0.32,0.77,-0.56); // rotating 0,0,-1 by 50 about x then 330 about y
+        //light.position.set(0.32,0.77,-0.56); // rotating 0,0,-1 by 50 about x then 330 about y
+        light.position.set(0.56,-0.32,0.77);
         scene.add(light);
         scene.add(new THREE.AmbientLight(0x404040));
 
         camera.position.copy(relativeCamPos);
-        camera.rotation.x = Math.PI / 4;
         camera.lookAt(new THREE.Vector3(0,0,0));
-        module.rotateCamera(10);
+        module.rotateCamera(-10);
 
-        plane.rotation.x += Math.PI / 2;
+        //plane.rotation.x += Math.PI / 2;
         robot.position.copy(robotOffset);
 
         clock.start();
@@ -75,9 +84,9 @@ var RuthefjordDisplay = (function() {
 
     function render() {
         var t = clock.getElapsedTime();
-        var y = WobbleMagnitude * Math.sin(t * 4 * Math.PI / WobblePeriod);
-        var x = WobbleMagnitude * Math.cos(t * 2 * Math.PI / WobblePeriod);
-        var v = new THREE.Vector3(x, y, 0);
+        var z = WobbleMagnitude * Math.sin(t * 4 * Math.PI / WobblePeriod);
+        var y = WobbleMagnitude * Math.cos(t * 2 * Math.PI / WobblePeriod);
+        var v = new THREE.Vector3(0, y, z);
 
         camera.position.copy( v.add(relativeCamPos).add(robot.position) );
         camera.lookAt(robot.position);
@@ -86,29 +95,43 @@ var RuthefjordDisplay = (function() {
         requestAnimationFrame(render);
     }
 
+    // until the Unity backend is discarded, we have to transform the axes it uses to the axes we use
+    function YZXFromXYZ(arr) {
+        /**
+         * input is:
+         * positive x is right
+         * positive y is up
+         * positive z is forward
+         */
+        return [arr[2], -arr[0], arr[1]];
+    }
+
     // grid is int array where each set of 4 ints is x,y,z,color of a cube
-    // CURRENTLY IGNORES CUBE COLOR
     module.setWorld = function(bot, grid) {
         // set robot position and direction
-        robot.position.fromArray(bot.pos).add(robotOffset);
+        robot.position.fromArray(YZXFromXYZ(bot.pos)).add(robotOffset);
         var dir = new THREE.Vector3();
-        dir.fromArray(bot.dir);
-        robot.quaternion.setFromUnitVectors(new THREE.Vector3( 0, 0, -1 ), dir); // 0,0,-1 is default direction
+        dir.fromArray(YZXFromXYZ(bot.dir));
+        robot.quaternion.setFromUnitVectors(new THREE.Vector3( 1, 0, 0 ), dir); // 1,0,0 is default direction
 
         // set cubes
-        // clear previous cubes
-        cubes.forEach(function (cube) {
-            scene.remove(cube);
+        // clear previous cubes, reset counts
+        cubeColors.forEach(function (color) {
+            cubes[color].meshes.forEach(function (mesh) {
+                scene.remove(mesh);
+            });
+            cubes[color].count = 0;
         });
-        // prep cube buffer
-        while (cubes.length < grid.length / 4) {
-            cubes.push(new THREE.Mesh(cubeGeo, cubeMats[1]));
-        }
         // add cubes to scene
         for(var i = 0; i < grid.length; i += 4) {
-            var cube = cubes[i/4];
+            var color = cubeColors[grid[i+3] - 1]; // color parameters are 1-indexed
+            // add a new mesh, if necessary
+            if (cubes[color].count >= cubes[color].meshes.length) {
+                cubes[color].meshes.push(new THREE.Mesh(cubeGeo, cubeMats[grid[i+3]]));
+            }
+            var cube = cubes[color].meshes[(cubes[color].count)++];
             scene.add(cube);
-            cube.position.set(grid[i], grid[i+1], grid[i+2]).add(cubeOffset);
+            cube.position.fromArray(YZXFromXYZ([grid[i], grid[i+1], grid[i+2]])).add(cubeOffset);
         }
     };
 
@@ -119,6 +142,9 @@ var RuthefjordDisplay = (function() {
     };
 
     module.tiltCamera = function(degrees) {
+        if (Math.abs(degrees) > 10) {
+            console.warn("tilting by more than 10 degrees in a single step may bypass safeguards");
+        }
         var curDot = relativeCamPos.clone().normalize().dot(UP);
         if ((curDot > 0.05 || degrees > 0) && (curDot < 0.95 || degrees < 0)) {
             var q = new THREE.Quaternion();
