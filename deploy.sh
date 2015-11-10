4644fc0478c0e26602453ed5e464b2a652bed60b
Aaron Bauer <awb@cs.washington.edu>
2015-11-09 16:05:44 -0800

    added check to deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,12 @@ HOST=webdeploy@dragonarchitect.net
 WEBROOT=/srv/www/play/
 KEYFILE=deploykey
 
+if [ -z "$1" ]
+then
+    echo "No release name given"
+    exit 1
+fi
+
 ssh -i $KEYFILE $HOST "rm -rf $WEBROOT/$1"
 rsync -avz -e "ssh -i $KEYFILE" dist $HOST:$WEBROOT
 ssh -i $KEYFILE $HOST mv $WEBROOT/dist $WEBROOT/$1
