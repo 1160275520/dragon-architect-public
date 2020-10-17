ca48ab7d28969f0f8bd4d7d8342b654b4e24ea14
Ellie Mamantov <mamantove@carleton.edu>
2020-10-17 13:46:11 -0500

    creating new translate javascript file, but it currently doesn't work :(

---
+++ html/js/blockly/translate.js
@@ -0,0 +1,12 @@
+var RuthefjordTranslate = (function () {
+    "use strict";
+
+    self.logJson = function () {
+        //Changed
+        console.log("HELLLOOOOOOOOOOOO")
+        console.log(JSON.stringify(RuthefjordBlockly.getProgram()));
+
+    };
+
+    return self;
+}());
